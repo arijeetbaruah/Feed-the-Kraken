4b31d9c8ef79a1b56d4fc8b7372d7fa8e02c9a56
Arijeet <arijeetbaruah@gmail.com>
2023-11-11 13:49:19 +0530

    no message

--- Assets/Games/Scripts/UI/Screen/MainMenuUI.cs
+++ Assets/Games/Scripts/UI/Screen/MainMenuUI.cs
@@ -1,6 +1,8 @@
 using Baruah.Service;
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Baruah.UI
 {
@@ -8,11 +10,34 @@ namespace Baruah.UI
     {
         [SerializeField] private TextMeshProUGUI welcomePlayer;
         [SerializeField] private UIButton playerBtn;
+        [SerializeField] private UIButton quitBtn;
 
         private async void Start()
         {
             string playerName = await ServiceManager.Get<AuthSystem>().GetPlayerName();
             welcomePlayer.SetText($"Welcome: <color=red>{playerName}</color>");
+        }
+
+        private void OnEnable()
+        {
+            playerBtn.AddListener(PlayBtnClick);
+            quitBtn.AddListener(QuitBtnClick);
+        }
+
+        private void OnDisable()
+        {
+            playerBtn.RemoveListener(PlayBtnClick);
+            quitBtn.RemoveListener(QuitBtnClick);
+        }
+
+        private void PlayBtnClick()
+        {
+            SceneManager.LoadScene(3);
+        }
+
+        private void QuitBtnClick()
+        {
+            Application.Quit();
         }
     }
 }
